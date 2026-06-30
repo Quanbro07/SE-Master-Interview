d2299f2539073cc990a89c4664b7a403bde6aeaa
anhJ04 <hs.nguyenthanhtrong@gmail.com>
2026-06-30 10:11:45 +0700

    Create "frontend" folder
    Create "frontend" folder

---
+++ src/frontend/app/component/Log-in panel/Log-in Panel.jsx
@@ -0,0 +1,9 @@
+import UseState from "react";
+import "./Log-in Panel.css";
+
+const LoginPanel = ({ onLogin }) => {
+  const [username, setUsername] = UseState("");
+  const [password, setPassword] = UseState("");
+};
+
+export default LoginPanel;
